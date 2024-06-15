1b74a80dc4d21b267f4a386d66fa2a0d7c131845
mccf3797 <adrien.bufort@orange.com>
2024-06-15 09:20:28 +0200

    implementing fairqueue

--- scripts/training.py
+++ scripts/training.py
@@ -2,20 +2,91 @@
 Module to train the model
 """
 
+import time
+import os
+import datetime
+import sys
+import torch
+
+from lightning.pytorch.callbacks import Callback
+
 from matchingflowexp import datasets as ds
 from torch.utils.data import DataLoader
 import lightning.pytorch as pl
 from matchingflowexp.trainer_pl import FlowTrainer
 
-import torch
 
 torch.set_float32_matmul_precision("medium")
 
 CURRENT_DIR = "/home/"
 
+DIR_WEIGHTS = CURRENT_DIR + "models/"
+NOM_MODELE = "matchingflow"
+
+
+# Register callbacks
+class MyFairRequeue(Callback):
+    """
+    Callback to stop the training to requeue the job
+    """
+
+    def __init__(self, dir_weights, prefix, max_duration_seconds=5000):
+        self.max_duration_seconds = max_duration_seconds
+        self.dir = dir_weights
+        self.prefix = prefix
+        self.start_time = time.monotonic()
+
+    def on_train_batch_end(self, trainer_module, pl_module, _, __, ___):
+        """
+        Save the model and exit if the training time is too long.
+        exit(42) is a magic exit code for slurm to plan requeueing
+        """
+        if (
+            "SLURM_JOB_ID" in os.environ
+            and time.monotonic() - self.start_time
+            >= self.max_duration_seconds  # avoid requeueing too often
+        ):
+            print("Time's up, prepare for fair requeueing")
+
+            # we register the date of the current checkpoint
+            date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            path = os.path.join(self.dir, self.prefix + "_" + date + ".ckpt")
+
+            trainer_module.save_checkpoint(path)
+
+            path_pth = os.path.join(self.dir, self.prefix + "_" + date + ".pt")
+            torch.save(pl_module.state_dict(), path_pth)
+
+            sys.exit(42)  # magic exit code for slurm to plan requeueing
+
+
+def get_last_checkpoint(dir_weight, nom_model):
+    """
+    Function to retrieve the last checkpoint.
+    """
+    # we check if there is a checkpoint in the directory
+    list_fichiers = os.listdir(dir_weight)
+
+    # we filter the files to keep only the ones corresponding to the model
+    list_fichiers = [f for f in list_fichiers if f.startswith(nom_model)]
+
+    # we sort the files by date of creation
+    list_fichiers.sort(key=lambda x: os.path.getmtime(os.path.join(dir_weight, x)))
+
+    # filter : we keep only the files with the extension .ckpt
+    list_fichiers = [f for f in list_fichiers if f.endswith(".ckpt")]
+
+    # we get the last file
+    if len(list_fichiers) > 0:
+        print("checkpoint found")
+        return os.path.join(dir_weight, list_fichiers[-1])
+
+    return None
+
+
 if __name__ == "__main__":
     train_dataset = ds.ImageNet64(
-        root= CURRENT_DIR + "data",
+        root=CURRENT_DIR + "data",
         train=True,
         transform=ds.DEFAULT_TRANSFORM,
     )
@@ -31,9 +102,26 @@ if __name__ == "__main__":
     # model.compile()
 
     # wandb logger
-    logger = None# .loggers.WandbLogger(project="matchingflowimagenet")
+    logger = None  # .loggers.WandbLogger(project="matchingflowimagenet")
+
+    # get last checkpoint (check the NOM_MODELE and take the last created)
+    last_checkpoint = get_last_checkpoint(DIR_WEIGHTS, NOM_MODELE)
+
+    # if there is a checkpoint, load it
+    if last_checkpoint is not None:
+        model.load_state_dict(torch.load(last_checkpoint)["state_dict"])
+
+        # define the checkpoint callback
+    checkpoint_model = MyFairRequeue(
+        DIR_WEIGHTS,
+        NOM_MODELE,
+        max_duration_seconds=5000,
+    )
 
     trainer = pl.Trainer(
-        max_time={"hours": 5}, logger=logger, gradient_clip_val=1.0, precision="bf16-mixed"
+        max_time={"hours": 48},
+        logger=logger,
+        gradient_clip_val=1.0,
+        precision="bf16-mixed",
     )
-    trainer.fit(model, train_loader)
+    trainer.fit(model, train_loader, ckpt_path=last_checkpoint)
